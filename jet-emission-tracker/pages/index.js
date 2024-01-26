2dae8217354a119065c9123e946fae2f0b5e1aaf
aidantsaunders <aidantsaunders@gmail.com>
2024-01-26 17:14:28 -0500

    Inital next.js setup

---
+++ jet-emission-tracker/pages/index.js
@@ -0,0 +1,18 @@
+import { Inter } from "next/font/google";
+import Link from 'next/link';
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <main
+      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    >
+      <div>
+        <Link href='/tracker'>
+          Jet Emission Tracker
+        </Link>
+      </div>
+    </main>
+  );
+}
---
+++ jet-emission-tracker/pages/tracker.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+
+export default function tracker() {
+    return(
+        <main>
+            <div>
+                Yeah I be trackin
+            </div>
+        </main>
+    )
+}
